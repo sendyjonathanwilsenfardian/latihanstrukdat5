a78b037e1152241c787d52f4a7a70adf2b73950f
sendyjonathanwilsenfardian <128010715+sendyjonathanwilsenfardian@users.noreply.github.com>
2023-05-25 09:20:00 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author joo
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(int data) {
+        this.data = data;
+    }
+}
